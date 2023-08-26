540712753850c1167ca0be113c96e5fb439c4804
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-26 21:28:13 +0200

    Code adaption for fj-daogen-maven-plugin

--- fj-daogen-legacy-v0-4-2-7/src/main/java/org/fugerit/java/daogen/base/config/DaogenFacade.java
+++ fj-daogen-legacy-v0-4-2-7/src/main/java/org/fugerit/java/daogen/base/config/DaogenFacade.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import org.fugerit.java.core.cfg.ConfigException;
 import org.fugerit.java.core.cfg.xml.FactoryType;
@@ -69,9 +70,23 @@ public class DaogenFacade {
 	}
 	
 	
-	public static void generate( InputStream fis ) throws ConfigException {
+	public static void generate( InputStream fis, Properties overrideProperties ) throws ConfigException {
 		try {
 			DaogenCatalogConfig daogenConfig = DaogenCatalogConfig.loadConfig( fis );
+			if ( overrideProperties != null ) {
+				logger.info( "override properties -> {}", overrideProperties );
+				for ( Object k : overrideProperties.keySet() ) {
+					String key = k.toString();
+					String value = overrideProperties.getProperty(key);
+					logger.info( "ovverride {} -> {}", key, value );
+					daogenConfig.getGeneralProps().setProperty(key, value);
+				}
+				daogenConfig.getGeneralProps().keySet().stream().sorted().forEach( 
+						k -> logger.info( "prop key : {} value : {}", k, daogenConfig.getGeneralProps().getProperty( k.toString() ) ) );
+			}
+			for ( DaogenGeneratorCatalog generatorCatalog : daogenConfig.getGeneratorCatalogs() ) {
+				generate(daogenConfig, generatorCatalog);
+			}
 			for ( DaogenGeneratorCatalog generatorCatalog : daogenConfig.getGeneratorCatalogs() ) {
 				generate(daogenConfig, generatorCatalog);
 			}
